ecea50d11f468e157137a54583f2f5f3f8ca1313
unknown <egor.kompaniets@gmail.com>
2023-04-04 08:16:41 +0300

    Created separate file for script

---
+++ Lesson21/script.js
@@ -0,0 +1,5 @@
+let firstNumber;
+let secondNumber;
+firstNumber = prompt("Enter first number: ");
+secondNumber = prompt("Enter second number: ");
+alert("Sum: " + (Number(firstNumber) + Number(secondNumber)) + "\nDifference: " + (Number(firstNumber) - Number(secondNumber)) + "\nProduct: " + (Number(firstNumber) * Number(secondNumber)) + "\nQuotient: " + (Number(firstNumber) / Number(secondNumber)));
