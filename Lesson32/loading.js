27e08edec0a3ac4e2f4e8e30a88e3b56f352e7ec
unknown <egor.kompaniets@gmail.com>
2023-04-18 00:46:24 +0300

    added repos list

--- Lesson32/loading.js
+++ Lesson32/loading.js
@@ -1,9 +1,14 @@
 const loader = document.querySelector('.loader');
+loader.innerHTML = '';
 
-loader.addEventListener("load", fetchLoaderAsync);
+loader.addEventListener('load', fetchLoaderAsync);
 
-async function etchLoaderAsync() {
-  const loader = await fetch('https://api.github.com/users/iGeorgeUA/repos');
+async function fetchLoaderAsync() {
+  const loading = await fetch('https://api.github.com/users/iGeorgeUA/repos');
+  const projects = await loading.json();
+
+  projects.forEach(project => {
     loader.addItem(project.full_name, project.html_url, project.description);
     loader.printItems();
+  });
 }
