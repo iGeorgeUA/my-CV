e7ef3dff7710bd61653f8bd92f6be4e8688b166e
unknown <egor.kompaniets@gmail.com>
2023-04-26 13:16:09 +0300

    added repos list

--- Lesson32/loading.js
+++ Lesson32/loading.js
@@ -1,35 +1,48 @@
-const fetchLoader = document.querySelector('fetchLoader');
+const fetchLoader = document.querySelector('.fetchLoader');
+/*
+fetch('https://api.github.com/users/iGeorgeUA/repos')
+  .then(res => res.json())
+  .then(data => console.log(data))
 
+fetch('https://jsonplaceholder.typicode.com/todos/')
+  .then(res => res.json())
+  .then(data => console.log(data))
+*/
 function ProjectList() {
   this.projects = [];
 }
 
 ProjectList.prototype.addItem = function (item) {
   this.projects.push({
-      item,
-      isDone: false
+      item
   });
 }
 
 ProjectList.prototype.printItems = function () {
+  fetchLoader.innerHTML = '';
+
   for (let i = 0; i < this.projects.length; i++) {
-      const p = document.createElement('p');
-      p.setAttribute('data-project', i);
-      p.textContent = this.projects[i].item;
-      p.appendChild(p);
+    const p = document.createElement('p');
+    p.setAttribute('data-project', i);
+    p.textContent = this.projects[i].item;
+
+    fetchLoader.appendChild(p);
   }
 }
 
 const projectList = new ProjectList()
-
-fetchLoader.addEventListener('onload', fetchLoaderAsync);
+window.addEventListener('load', fetchLoaderAsync);
 
 async function fetchLoaderAsync() {
   const loaderRaw = await fetch('https://api.github.com/users/iGeorgeUA/repos');
   const projects = await loaderRaw.json();
 
   projects.forEach(project => {
-    projectList.addItem(project.full_name, project.html_url, project.description);
+    projectList.addItem(project.full_name);
+    projectList.addItem(project.html_url);
+    projectList.addItem(project.description);
+    console.log(projectList)
     projectList.printItems();
+
   });
 }
