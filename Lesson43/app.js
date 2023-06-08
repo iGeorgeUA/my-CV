57db94c38617b90091e6a6009c9fda76d6769a1e
unknown <egor.kompaniets@gmail.com>
2023-06-08 21:31:55 +0300

    Initial

---
+++ Lesson43/app.js
@@ -0,0 +1,18 @@
+const { performance } = require('perf_hooks');
+
+let t0 = performance.now();
+let products = {};
+for (let i = 1; i <= 1000; i++) {
+    products[`product${i}`] = i;
+}
+let t1 = performance.now();
+
+console.log(`Time to create object: ${(t1 - t0)} milliseconds.`);
+
+t0 = performance.now();
+let price = products['product50'];
+t1 = performance.now();
+
+console.log(`Time to access 50th element: ${(t1 - t0)} milliseconds.`);
+
+console.log(products);
