f8840bdb355ca2b406bcde53fb1f3d0102a2c9aa
unknown <egor.kompaniets@gmail.com>
2023-06-21 18:36:14 +0300

    refactor

---
+++ Lesson46/httpsServer/server.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const app = express();
+app.use(express.json());
+
+const { createAccountHandler, getAccountsHandler, getAccountByNameHandler, updateAccountHandler, deleteAccountHandler, generateTokenHandler } = require('./accountController');
+
+app.post('/accounts', createAccountHandler);
+
+app.get('/accounts', getAccountsHandler);
+
+app.get('/accounts/:name', getAccountByNameHandler);
+
+app.put('/accounts/:name', updateAccountHandler);
+
+app.delete('/accounts/:name', deleteAccountHandler);
+
+app.post('/token', generateTokenHandler);
+
+const port = process.env.PORT || 1337;
+app.listen(port, () => console.log(`Listening on port ${port}...`));
