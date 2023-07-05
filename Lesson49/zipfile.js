c7de8364cdee62a0580815ea4e70547cf78d29d5
unknown <egor.kompaniets@gmail.com>
2023-07-05 16:53:55 +0300

    bugged

--- Lesson49/zipfile.js
+++
@@ -1,7 +0,0 @@
-const fs = require('fs');
-const zlib = require('zlib');
-
-const r = fs.createReadStream('book.txt');
-const z = zlib.createGzip();
-const w = fs.createWriteStream('book.txt.gz');
-r.pipe(z).pipe(w);
